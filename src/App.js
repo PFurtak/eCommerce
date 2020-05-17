4b3e0da2521bba4b9affe39119985948a1193884
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-17 19:38:00 -0400

    Created auth page components, created sign in form and set up its state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './components/pages/homepage/Home';
 import Shop from './components/pages/shoppage/Shop';
 import Header from './components/header/Header';
+import SignInAndSignUpPage from './components/pages/auth/SignInAndSignUp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
