d1d14150d801365314f9ff5696531ced785de7c8
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-17 18:31:29 -0400

    Created header component, styled header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/homepage/Home';
 import Shop from './components/pages/shoppage/Shop';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/shop' component={Shop} />
