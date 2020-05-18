3d52bf4fa67967744ffed497ca83c99ceac0634c
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-18 13:12:37 -0400

    Refactored app.js into class component to allow for state. Added auth observer from firebase, and how it should handle auth changes in a users session

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { auth } from './firebase/firebase.utils';
 import './App.css';
 import Home from './components/pages/homepage/Home';
 import Shop from './components/pages/shoppage/Shop';
 import Header from './components/header/Header';
 import SignInAndSignUpPage from './components/pages/auth/SignInAndSignUp';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
