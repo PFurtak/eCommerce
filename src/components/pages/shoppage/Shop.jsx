ae703dd5c661f4f92b2144c6bd4b30eac851e54a
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-26 00:17:04 -0400

    Migrated collection overview data to its own component

--- src/components/pages/shoppage/Shop.jsx
+++ src/components/pages/shoppage/Shop.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import Collection from '../../collection/Collection';
-import { selectCollections } from '../../../redux/shop/shop.selector';
+import CollectionsOverview from '../../collections-overview/CollectionsOverview';
 
 const Shop = ({ collections }) => {
   return (
     <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <Collection key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(Shop);
+export default Shop;
