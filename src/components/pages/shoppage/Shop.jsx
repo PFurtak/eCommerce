8cdf443dd6df0f0391e3ea051ee39ed40bedac28
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-26 01:00:00 -0400

    Created category page, Dynamically calling route to category page, Returning the correct items per category page

--- src/components/pages/shoppage/Shop.jsx
+++ src/components/pages/shoppage/Shop.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../collections-overview/CollectionsOverview';
+import CategoryPage from '../category/Category';
 
-const Shop = ({ collections }) => {
+const Shop = ({ match }) => {
   return (
     <div className='shop-page'>
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
     </div>
   );
 };
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,23 @@
 import { createSelector } from 'reselect';
 
+const CATEGORY_ID_MAP = {
+  hats: 1,
+  sneaker: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCategory = (categoryUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === CATEGORY_ID_MAP[categoryUrlParam]
+    )
+  );
